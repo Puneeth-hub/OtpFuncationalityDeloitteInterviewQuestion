b504e2fa95180b258ab8603a8a95e4233406a3f9
Puneeth-hub <puneethkumargajjela@gmail.com>
2024-08-17 12:01:03 +0530

    twoway

--- OtpFunctionality.js
+++ OtpFunctionality.js
@@ -7,3 +7,19 @@ function generateOtp(){
 }
 console.log(generateOtp())
   
+
+
+//Another Approach   
+
+
+function generateOTP(length = 4) {
+    let otp = '';
+    for (let i = 0; i < length; i++) {
+        otp += Math.floor(Math.random() * 10);
+    }
+    return otp;
+}
+console.log(generateOTP())
+
+
+
